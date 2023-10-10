57132604bfb15e002450208de52d74c4f947d0c0
JustinWayland <justintwayland+github@gmail.com>
2023-10-10 14:37:16 -0400

    Add skeletons for functions I know I'll need

--- src/expandupdate.nim
+++ src/expandupdate.nim
@@ -1,5 +1,21 @@
 import cligen
+import std/[lexbase, streams]
 
-func expandupdate(text_tables: seq[string], output: string, file: string): int = discard
+type
+    UpdateParserState = enum
+        statePlainText, statePotentialExpansion
+    UpdateParser = object of BaseLexer
+        curtext: string
+        state: UpdateParserState
+
+proc open(L: var UpdateParser, input: Stream) =
+    lexbase.open(L, input)
+    L.curtext = ""
+    L.state = statePlainText
+
+proc getNextFragment(parser: var UpdateParser): string = discard
+
+proc expandupdate(text_tables: seq[string], output: string, file: string): int = discard
+
 
 dispatch expandupdate
